dc07683b76f7f518ed0ef706a436d86d61202708
soekhant01 <soekhant.dev@gmail.com>
2025-06-30 23:00:50 +0630

    financeSection_added

--- app/src/main/java/com/example/bankingui/MainActivity.kt
+++ app/src/main/java/com/example/bankingui/MainActivity.kt
@@ -64,7 +64,7 @@ fun HomeScreen(){
             WalletSection()
             CardsSection()
             Spacer(modifier = Modifier.height(16.dp))
-//        FinanceSection()
+        FinanceSection()
 //        CurrenciesSection()
         }
 
